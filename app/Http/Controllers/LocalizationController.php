f2f15931b9d663576d886e3f52170dd95718d57c
lutfiddin <shamsidinovlutfiddin@gmail.com>
2021-04-02 17:01:49 +0500

    localization controller

---
+++ app/Http/Controllers/LocalizationController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LocalizationController extends Controller
+{
+     public function index($locale) {
+         \App::setLocale($locale);
+        // store the locale in session so that the middleware can register it
+        session()->put('locale', $locale);
+        return redirect()->back();
+    }
+}
